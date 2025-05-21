70bddd0198623c79596308e3d36a8842f2a9ad5e
sukumar624 <sukumarmajhi624@gmail.com>
2025-05-21 15:21:12 +0530

    product page create

--- src/features/productSlice.js
+++ src/features/productSlice.js
@@ -1,42 +1,47 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const fetchProducts = createAsyncThunk('products/fetchProducts', async (_, { rejectWithValue }) => {
-    try {
-        const response = await axios.get('https://fakestoreapi.com/products');
-        console.log('response', response);
-
-        return response.data
-    } catch (error) {
-        return rejectWithValue(error.response?.data || error.message)
+// Async thunk
+export const fetchProducts = createAsyncThunk(
+    'products/fetchProducts',
+    async (_, { rejectWithValue }) => {
+        try {
+            const response = await axios.get('https://fakestoreapi.com/products');
+            console.log('response', response);
+            return response.data;
+        } catch (error) {
+            return rejectWithValue(error.response?.data || error.message);
+        }
     }
-})
+);
 
-
+// Initial state
 const initialState = {
     product: [],
     loading: false,
     error: null
-}
+};
+
+// Slice
 const productSlice = createSlice({
     name: 'products',
     initialState,
-    reducer: {},
+    reducers: {}, // ✅ fixed typo
     extraReducers: (builder) => {
-        builder.addCase(fetchProducts.pending, (state) => {
-            state.loading = true;
-            state.error = null;
-        })
-        builder.addCase(fetchProducts.fulfilled, (state, action) => {
-            state.loading = false;
-            state.product = action.payload;
-        })
-        builder.addCase(fetchProducts.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.payload || 'Something went wrong'
-        })
+        builder
+            .addCase(fetchProducts.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(fetchProducts.fulfilled, (state, action) => {
+                state.loading = false;
+                state.product = action.payload;
+            })
+            .addCase(fetchProducts.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload || 'Something went wrong';
+            });
     }
-
-})
+});
 
 export default productSlice.reducer;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { fetchProducts } from "../features/productSlice";
+import { FaRupeeSign } from "react-icons/fa";
+
 
 const Products = () => {
-    const value = useSelector((state) => state.products)
-    const dispatch = useDispatch()
-    console.log('fvalue', value);
+    const { product, loading, error } = useSelector((state) => state.products)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchProducts())
+    }, [dispatch])
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+    if (error) {
+        return <div>{error}</div>
+    }
 
     return (
-        <div>Products</div>
+        <div>
+            <div className="container">
+                <div className="row g-3">
+                    {
+                        product.map((item) => (
+                            <div className="col-sm-3" key={item.id}>
+                                <div className="single_product">
+                                    <div className="card">
+                                        <div className="product_img">
+                                            <img src={item.image} className="img-fluid" alt="" />
+                                        </div>
+                                        <div className="product_info">
+                                            <h4>{item.title}</h4>
+                                            <p>
+                                                <FaRupeeSign /> {item.price}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
     )
 }
 
